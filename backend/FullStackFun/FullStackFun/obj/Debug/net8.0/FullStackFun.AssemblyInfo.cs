7d9db4393465506fbc7ae6bd2dae90360fd93ad0
tobwhit <tobin.whitworth@gmail.com>
2026-03-10 16:04:45 -0600

    various files

--- backend/FullStackFun/FullStackFun/obj/Debug/net8.0/FullStackFun.AssemblyInfo.cs
+++ backend/FullStackFun/FullStackFun/obj/Debug/net8.0/FullStackFun.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FullStackFun")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33ad21db0bed743c7cc9e1aa019aee1abf39a8d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FullStackFun")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FullStackFun")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
